2b8d97a7f8bf2ee6be221ef7f20e08c69ea0bbe9
Shunya <luque.viictor@gmail.com>
2022-01-17 20:55:25 +0100

    Add config.py
    So far it only includes configurator for Paypal importer

---
+++ config.py
@@ -0,0 +1,15 @@
+import os, sys
+
+# beancount doesn't run from this directory
+sys.path.append(os.path.dirname(__file__))
+
+# importers located in the importers directory
+from importers import paypal
+
+# Setting this variable provides a list of importer instances.
+CONFIG = [
+    paypal.PaypalImporter('Assets:Online:Paypal:Checking', '0000'),
+]
+
+# Override the header on extracted text (if desired).
+#extract.HEADER = ';; -*- mode: org; mode: beancount; coding: utf-8; -*-\n'
