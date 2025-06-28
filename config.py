eee7899a090740022db4f3733ded1b6bd9435ddf
Victor Luque <luque.viictor@gmail.com>
2025-06-28 18:16:41 +0200

    modify config for v3

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
-import os, sys
+import sys
+from os import path
 
 # beancount doesn't run from this directory
-sys.path.append(os.path.dirname(__file__))
+sys.path.insert(0, path.join(path.dirname(__file__)))
 
 # importers located in the importers directory
-from importers import paypal, caixabank
+from importers import caixabank
 
 # Setting this variable provides a list of importer instances.
 CONFIG = [
-    paypal.PaypalImporter('Assets:Online:Paypal:Checking'),
-    caixabank.CaixaBankImporter('Assets:EU:CaixaBank:Checking')
+    #paypal.PaypalImporter('Assets:Online:Paypal:Checking'),
+    caixabank.Importer('Assets:EU:CaixaBank:Checking')
 ]
 
 # Override the header on extracted text (if desired).
 #extract.HEADER = ';; -*- mode: org; mode: beancount; coding: utf-8; -*-\n'
+
+
+if __name__ == "__main__":
+    ingest = beangulp.Ingest(CONFIG)
+    ingest()
