02954a8370478bca1e403deec2f4d97e147c1471
Victor Luque <luque.viictor@gmail.com>
2025-07-06 13:18:02 +0200

    bean: working config (no postings)

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
 from os import path
 
+import beangulp
+
 # beancount doesn't run from this directory
 sys.path.insert(0, path.join(path.dirname(__file__)))
 
@@ -10,7 +12,12 @@ from importers import caixabank
 # Setting this variable provides a list of importer instances.
 CONFIG = [
     #paypal.PaypalImporter('Assets:Online:Paypal:Checking'),
-    caixabank.Importer('Assets:EU:CaixaBank:Checking')
+    caixabank.Importer(
+        {
+            "main_account": "Assets:EU:CaixaBank:Checking",
+            "account_number": "0101278127",
+        }
+    )
 ]
 
 # Override the header on extracted text (if desired).
